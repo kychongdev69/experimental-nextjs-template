ca6a3640911677c85f7fc73071b1a581c3912169
kychongdev69 <kychongdev@vvinners.com>
2023-08-09 14:11:30 +0800

    Reorganize folder structure

---
+++ src/app/(routes)/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "DreamSoft",
+  description: "A site that empower user",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
